19f32012d8867c500db3ffb8efc21c4eadbb598b
AlexKasema <lexxmulwa@gmail.com>
2023-12-06 18:32:51 +0300

    Price range slider design and setup

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,10 +1,11 @@
 from . models import Product, Category, Vendor, ProductImages, CartOrder, CartOrderItems, ProductReview, WishList, Address
-
+from django.db.models import Min, Max
 
 
 def default(request):
     categories = Category.objects.all()
     vendors = Vendor.objects.all()
+    min_max_price = Product.objects.aggregate(Min("price"), Max("price"))
 
     try:
         address = Address.objects.get(user=request.user)
@@ -12,5 +13,5 @@ def default(request):
         address = None
 
     return {
-        'categories': categories, 'address': address, 'vendors': vendors
+        'categories': categories, 'address': address, 'vendors': vendors, 'min_max_price': min_max_price
     }
